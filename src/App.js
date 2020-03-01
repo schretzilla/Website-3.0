01f5a6662bf87a2524423365cd07e00b8c07b064
schretzilla <schretzilla@gmail.com>
2020-03-01 13:01:15 -0800

    Refactored app and intro to include experience

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Container from 'react-bootstrap/Container';
 
 import Intro from './Introduction/Intro';
+import ExperienceView from './Experience/ExperienceView';
+
 function App() {
   return (
-    <>
-      <Intro></Intro>
-    </>
+    <Container>
+      <Intro />
+      <ExperienceView />
+    </Container>
   );
 }
 
--- src/Introduction/Intro.js
+++ src/Introduction/Intro.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
@@ -8,59 +7,57 @@ import profilePhoto from '../Images/Profile-Photo.png';
 import githubIcon from '../Images/github-icon.png';
 import linkedInIcon from '../Images/linkedin-logo.png';
 
-const intro = props => {
+const Intro = props => {
   return (
     <>
-      <Container>
-        <Row>
-          <Col sm={12} md={6}>
-            <img
-              className="img-fluid"
-              src={profilePhoto}
-              alt="scott-schretzenmaier-profile"
-            />
-          </Col>
-          <Col sm={12} md={6}>
-            <h1>Hi, I'm Scott</h1>
-            <p>
-              I'm a motivated guy who loves building awesome defense software. I
-              prefer backend development but I dabble with a bit of everything.
-              I'm also a big fan of agile processes, both in development and in
-              life.
-            </p>
+      <Row>
+        <Col sm={12} md={6}>
+          <img
+            className="img-fluid"
+            src={profilePhoto}
+            alt="scott-schretzenmaier-profile"
+          />
+        </Col>
+        <Col sm={12} md={6}>
+          <h1>Hi, I'm Scott</h1>
+          <p>
+            I'm a motivated guy who loves building awesome defense software. I
+            prefer backend development but I dabble with a bit of everything.
+            I'm also a big fan of agile processes, both in development and in
+            life.
+          </p>
 
-            <ul className="list-inline">
-              <li>
-                <strong>Email:</strong> schretzy929@gmail.com
-              </li>
-              <li>
-                <strong>Location:</strong> Los Angeles, CA
-              </li>
-              <li>
-                <strong>Resume:</strong> google drive live resume folder
-              </li>
-            </ul>
+          <ul className="list-inline">
+            <li>
+              <strong>Email:</strong> schretzy929@gmail.com
+            </li>
+            <li>
+              <strong>Location:</strong> Los Angeles, CA
+            </li>
+            <li>
+              <strong>Resume:</strong> google drive live resume folder
+            </li>
+          </ul>
 
-            <a
-              href="https://github.com/schretzilla"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={githubIcon} alt="https://github.com/schretzilla" />
-            </a>
+          <a
+            href="https://github.com/schretzilla"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={githubIcon} alt="https://github.com/schretzilla" />
+          </a>
 
-            <a
-              href="http://www.linkedin.com/in/scott-schretzenmaier-26ab1178"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={linkedInIcon} alt="linked in logo" />
-            </a>
-          </Col>
-        </Row>
-      </Container>
+          <a
+            href="http://www.linkedin.com/in/scott-schretzenmaier-26ab1178"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedInIcon} alt="linked in logo" />
+          </a>
+        </Col>
+      </Row>
     </>
   );
 };
 
-export default intro;
+export default Intro;
