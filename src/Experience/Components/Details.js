68c6359ba9914c2377a9bccf870f326c6303ed51
schretzilla <schretzilla@gmail.com>
2020-03-01 13:58:01 -0800

    Moved all style into style obj

--- src/Experience/Components/Details.js
+++ src/Experience/Components/Details.js
@@ -3,54 +3,51 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const companyName = {
-  fontWeight: 'normal',
-  color: '#000000',
-  letterSpacing: '2pt',
-  wordSpacing: '2pt',
-  fontSize: '50px',
-  textAlign: 'left',
-  fontFamily: 'georgia',
-  lineHeight: '1'
-};
-
-const jobTitle = {
-  fontWeight: 'normal',
-  color: '#000000',
-  wordSpacing: '2pt',
-  fontSize: '40px',
-  textAlign: 'left',
-  fontFamily: 'georgia'
-};
-
-const dateStyle = {
-  fontWeight: 'normal',
-  color: '#000000',
-  marginTop: '10px',
-  fontSize: '30px',
-  fontFamily: 'georgia',
-  lineHeight: '1'
-};
-
-const jobDescription = {
-  fontSize: '25px',
-  color: '#515151'
-};
-
-const teamName = {
-  fontWeight: 'normal',
-  color: '#000000',
-  fontSize: '30px',
-  textAlign: 'left',
-  fontFamily: 'georgia'
-};
-
-const jobKeyPoints = {
-  fontWeight: 'normal',
-  color: '#000000',
-  fontSize: '20px',
-  textAlign: 'left',
-  fontFamily: 'georgia'
+const style = {
+  companyName: {
+    fontWeight: 'normal',
+    color: '#000000',
+    letterSpacing: '2pt',
+    wordSpacing: '2pt',
+    fontSize: '50px',
+    textAlign: 'left',
+    fontFamily: 'georgia',
+    lineHeight: '1'
+  },
+  jobTitle: {
+    fontWeight: 'normal',
+    color: '#000000',
+    wordSpacing: '2pt',
+    fontSize: '40px',
+    textAlign: 'left',
+    fontFamily: 'georgia'
+  },
+  dateStyle: {
+    fontWeight: 'normal',
+    color: '#000000',
+    marginTop: '10px',
+    fontSize: '30px',
+    fontFamily: 'georgia',
+    lineHeight: '1'
+  },
+  jobDescription: {
+    fontSize: '25px',
+    color: '#515151'
+  },
+  teamName: {
+    fontWeight: 'normal',
+    color: '#000000',
+    fontSize: '30px',
+    textAlign: 'left',
+    fontFamily: 'georgia'
+  },
+  jobKeyPoints: {
+    fontWeight: 'normal',
+    color: '#000000',
+    fontSize: '20px',
+    textAlign: 'left',
+    fontFamily: 'georgia'
+  }
 };
 
 const Details = props => {
@@ -64,25 +61,29 @@ const Details = props => {
     'This is a list element saying more of what you did'
   ];
 
-  const keyPointElement = keyPoints.map(keyPoint => {
-    return <li style={jobKeyPoints}>{keyPoint}</li>;
+  const keyPointElement = keyPoints.map((keyPoint, i) => {
+    return (
+      <li key={`key-point-${company}-${i}`} style={style.jobKeyPoints}>
+        {keyPoint}
+      </li>
+    );
   });
   return (
     <>
       <Row>
         <Col sm={8}>
-          <h2 style={companyName}>{company}</h2>
-          <h2 style={jobTitle}>{title}</h2>
+          <h2 style={style.companyName}>{company}</h2>
+          <h2 style={style.jobTitle}>{title}</h2>
         </Col>
         <Col sm={4}>
-          <h2 style={dateStyle}>{dates}</h2>
+          <h2 style={style.dateStyle}>{dates}</h2>
         </Col>
       </Row>
 
       <Row>
         <Col sm={12}>
-          <p style={jobDescription}>{description}</p>
-          <p style={teamName}>{teams}</p>
+          <p style={style.jobDescription}>{description}</p>
+          <p style={style.teamName}>{teams}</p>
         </Col>
 
         <ul>{keyPointElement} </ul>
