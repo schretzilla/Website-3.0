1bc60af6dd903132d202dae50a62b5e288b0e5b9
schretzilla <schretzilla@gmail.com>
2020-03-01 13:49:50 -0800

    Moved all data into variables

--- src/Experience/Components/Details.js
+++ src/Experience/Components/Details.js
@@ -54,34 +54,38 @@ const jobKeyPoints = {
 };
 
 const Details = props => {
+  const company = 'Company Name/ logo';
+  const title = 'Job title';
+  const dates = 'from date / to date';
+  const description = 'Job highlight or description gos on this line';
+  const teams = 'Team Name Goes on this line';
+  const keyPoints = [
+    'This is a list element saying what you did',
+    'This is a list element saying more of what you did'
+  ];
+
+  const keyPointElement = keyPoints.map(keyPoint => {
+    return <li style={jobKeyPoints}>{keyPoint}</li>;
+  });
   return (
     <>
       <Row>
         <Col sm={8}>
-          <h2 style={companyName}>Company Name/ logo</h2>
-          <h2 stly={jobTitle}>Job title</h2>
+          <h2 style={companyName}>{company}</h2>
+          <h2 style={jobTitle}>{title}</h2>
         </Col>
         <Col sm={4}>
-          <h2 style={dateStyle}>from date / to date</h2>
+          <h2 style={dateStyle}>{dates}</h2>
         </Col>
       </Row>
 
       <Row>
         <Col sm={12}>
-          <p style={jobDescription}>
-            Job highlight or description gos on this line
-          </p>
-          <p style={teamName}>Team Name Goes on this line</p>
+          <p style={jobDescription}>{description}</p>
+          <p style={teamName}>{teams}</p>
         </Col>
 
-        <ul>
-          <li style={jobKeyPoints}>
-            This is a list element saying what you did
-          </li>
-          <li style={jobKeyPoints}>
-            This is a list element saying more of what you did
-          </li>
-        </ul>
+        <ul>{keyPointElement} </ul>
       </Row>
     </>
   );
