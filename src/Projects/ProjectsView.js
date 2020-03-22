207410fa1c9e8716eeb742f28b806defb89203e2
greatscott <scott.schretzenmaier@ngc.com>
2020-03-22 11:24:46 -0700

    projects now populated by project data

--- src/Projects/ProjectsView.js
+++ src/Projects/ProjectsView.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import Row from 'react-bootstrap/Row';
+import Row from "react-bootstrap/Row";
 
-import Project from './components/Project';
+import Project from "./components/Project";
+
+import { projectData } from "../Data/Projects";
 
 const ProjectView = props => {
+  const projectSections = projectData.map((projectObj, i) => {
+    return (
+      <Project
+        key={projectObj.name + i}
+        projectData={projectObj}
+        indexNumber={i}
+      />
+    );
+  });
+
   return (
     <>
       <Row className="justify-content-center">
         <h2 className="subject-header">Projects</h2>
       </Row>
-      <Project />
+      {projectSections}
     </>
   );
 };
