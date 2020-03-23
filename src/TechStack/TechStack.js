cd3bb23f2b3f866d4ae0f3fa9b8536bec7b6e5b0
greatscott <scott.schretzenmaier@ngc.com>
2020-03-22 18:27:58 -0700

    added react and java + centered tech stack icons

--- src/TechStack/TechStack.js
+++ src/TechStack/TechStack.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-import cSharpIcon from '../Images/csharp.png';
-import bootstrapIcon from '../Images/bootstrap.png';
-import djangoIcon from '../Images/django.png';
-import javaScriptIcon from '../Images/javascript.png';
-import dotNetIcon from '../Images/microsoft-net.png';
-import pythonIcon from '../Images/python.png';
+import cSharpIcon from "../Images/csharp.png";
+import bootstrapIcon from "../Images/bootstrap.png";
+import djangoIcon from "../Images/django.png";
+import javaScriptIcon from "../Images/javascript.png";
+import dotNetIcon from "../Images/microsoft-net.png";
+import pythonIcon from "../Images/python.png";
+import reactIcon from "../Images/react-logo.svg";
+import javaIcon from "../Images/java-logo.png";
 
 const TechStack = props => {
+  const style = {
+    icon: {
+      maxHeight: "135px"
+    }
+  };
   return (
     <>
       <Row className="justify-content-center">
@@ -19,24 +26,54 @@ const TechStack = props => {
 
       <Row className="justify-content-center">
         <Col xs={4}>
-          <img src={cSharpIcon} alt="c#" />
+          <img src={cSharpIcon} alt="c#" className="rounded mx-auto d-block" />
         </Col>
         <Col xs={4}>
-          <img src={bootstrapIcon} alt="bootstrap css framework" />
+          <img
+            title="Bootstrap CSS"
+            src={bootstrapIcon}
+            style={style.icon}
+            alt="bootstrap css framework"
+            className="rounded mx-auto d-block"
+          />
         </Col>
         <Col xs={4}>
-          <img src={djangoIcon} alt="django" />
+          <img
+            title="React Framework"
+            src={reactIcon}
+            style={style.icon}
+            alt="django"
+            className="rounded mx-auto d-block"
+          />
         </Col>
       </Row>
       <Row>
         <Col xs={4}>
-          <img src={javaScriptIcon} alt="javascript" />
+          <img
+            title="JavaScript"
+            src={javaScriptIcon}
+            style={style.icon}
+            alt="javascript"
+            className="rounded mx-auto d-block"
+          />
         </Col>
         <Col xs={4}>
-          <img src={dotNetIcon} alt=".Net Framework" />
+          <img
+            title="Java"
+            src={javaIcon}
+            style={style.icon}
+            alt="Java"
+            className="rounded mx-auto d-block"
+          />
         </Col>
         <Col xs={4}>
-          <img src={pythonIcon} alt="python" />
+          <img
+            title="python"
+            src={pythonIcon}
+            style={style.icon}
+            alt="python"
+            className="rounded mx-auto d-block"
+          />
         </Col>
       </Row>
 
