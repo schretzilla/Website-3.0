1e82fce6537bc0a103eca683664c6e877e3f5099
greatscott <scott.schretzenmaier@ngc.com>
2020-03-29 10:38:09 -0700

    refactor: moved projects and experience to sub folders

--- src/Data/Experience.js
+++ src/Data/Experience.js
@@ -1,6 +1,6 @@
-import NorthropLogo from "../Images/northrop-logo.png";
-import UpmcLogo from "../Images/upmc_logo.jpg";
-import HikingLogo from "../Images/gone-hiking-logo.png";
+import NorthropLogo from "../Images/Experience/northrop-logo.png";
+import UpmcLogo from "../Images/Experience/upmc_logo.jpg";
+import HikingLogo from "../Images/Experience/gone-hiking-logo.png";
 export const experienceData = [
   {
     company: "Northrop Grumman",
