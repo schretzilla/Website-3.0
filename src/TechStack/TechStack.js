e56de1aa14add4b6882be9ac80bcabaf7483b81a
Scott Schretzenmaier <schretzilla@gmail.com>
2020-03-28 17:35:32 -0700

    Tech Stack Enhancements with Flippy (#17)
    * feat: added flippy dependency
    
    * feat: flippy animations added
    
    * fix: dynamic content sizing
    
    * refactor: moved all logos into sub folder
    
    * fix: standardized size of tech icon images
    
    * style: added font to description
    
    * refactor: tech icon style moved to constant
    
    * feat: added link to all tech stack items
    
    Co-authored-by: greatscott <scott.schretzenmaier@ngc.com>

--- src/TechStack/TechStack.js
+++ src/TechStack/TechStack.js
@@ -3,78 +3,85 @@ import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-import cSharpIcon from "../Images/csharp.png";
-import bootstrapIcon from "../Images/bootstrap.png";
-import javaScriptIcon from "../Images/javascript.png";
-import pythonIcon from "../Images/python.png";
-import reactIcon from "../Images/react-logo.svg";
-import javaIcon from "../Images/java-logo.png";
+import cSharpIcon from "../Images/tech-stack/csharp.png";
+import bootstrapIcon from "../Images/tech-stack/bootstrap.png";
+import javaScriptIcon from "../Images/tech-stack/javascript.png";
+import pythonIcon from "../Images/tech-stack/python.png";
+import reactIcon from "../Images/tech-stack/react.png";
+import javaIcon from "../Images/tech-stack/java.png";
 
 import Fade from "react-reveal/Fade";
 
+import Flippy, { FrontSide, BackSide } from "react-flippy";
+
+const style = {
+  flipCol: {
+    marginBottom: "30px"
+  },
+  techDescription: {
+    marginTop: "10%"
+  },
+  techIcon: {
+    maxWidth: "100%",
+    maxHeight: "100%"
+  }
+};
+
+const flipCard = (img, description, link) => {
+  return (
+    <Col xs={6} md={4} xl={2} style={style.flipCol}>
+      <Flippy flipOnHover={true} flipDirection="vertical">
+        <FrontSide>
+          <img
+            src={img}
+            title={description}
+            alt={description}
+            className="rounded mx-auto d-block"
+            style={style.techIcon}
+          />
+        </FrontSide>
+        <BackSide>
+          <p className="lead text-center" style={style.techDescription}>
+            <a href={link} target="_blank" rel="noopener noreferrer">
+              {description}
+            </a>
+          </p>
+        </BackSide>
+      </Flippy>
+    </Col>
+  );
+};
+
 const TechStack = props => {
-  const style = {
-    icon: {
-      maxHeight: "135px"
-    }
-  };
   return (
     <Fade bottom>
       <Row className="justify-content-center">
-        <h2 className="subject-header">My current tech stack</h2>
+        <h2 className="subject-header">Tech Stack</h2>
       </Row>
 
       <Row className="justify-content-center">
-        <Col xs={4}>
-          <img src={cSharpIcon} alt="c#" className="rounded mx-auto d-block" />
-        </Col>
-        <Col xs={4}>
-          <img
-            title="Bootstrap CSS"
-            src={bootstrapIcon}
-            style={style.icon}
-            alt="bootstrap css framework"
-            className="rounded mx-auto d-block"
-          />
-        </Col>
-        <Col xs={4}>
-          <img
-            title="React Framework"
-            src={reactIcon}
-            style={style.icon}
-            alt="django"
-            className="rounded mx-auto d-block"
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={4}>
-          <img
-            title="JavaScript"
-            src={javaScriptIcon}
-            style={style.icon}
-            alt="javascript"
-            className="rounded mx-auto d-block"
-          />
-        </Col>
-        <Col xs={4}>
-          <img
-            title="Java"
-            src={javaIcon}
-            style={style.icon}
-            alt="Java"
-            className="rounded mx-auto d-block"
-          />
-        </Col>
-        <Col xs={4}>
-          <img
-            title="python"
-            src={pythonIcon}
-            style={style.icon}
-            alt="python"
-            className="rounded mx-auto d-block"
-          />
-        </Col>
+        {flipCard(
+          cSharpIcon,
+          "C#",
+          "https://docs.microsoft.com/en-us/dotnet/csharp/"
+        )}
+        {flipCard(
+          bootstrapIcon,
+          "Bootstrap CSS Framework",
+          "https://getbootstrap.com/"
+        )}
+        {flipCard(reactIcon, "React Framework", "https://reactjs.org/")}
+        {flipCard(
+          javaScriptIcon,
+          "JavaScript",
+          "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+        )}
+        {flipCard(
+          javaIcon,
+          "Java",
+          "https://docs.oracle.com/javase/8/docs/technotes/guides/language/index.html"
+        )}
+        {flipCard(pythonIcon, "Python", "https://www.python.org/")}
       </Row>
 
       <hr className="featurette-divider" />
