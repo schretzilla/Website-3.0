e43d3a8d741fc86b38f951688aeb7b8b19ac934e
Scott Schretzenmaier <schretzilla@gmail.com>
2020-03-30 19:06:47 -0700

    refactor: modularize slider component (#27)
    * fix: project date is pushed to side of text
    
    * refactor: removed comments
    
    * refactor: modularized slider component
    
    * tests: updated snapshots
    
    Co-authored-by: greatscott <scott.schretzenmaier@ngc.com>

--- src/Components/ExtraActivities/components/ActivitySlider.js
+++ src/Components/ExtraActivities/components/ActivitySlider.js
@@ -25,48 +25,41 @@ export default class SimpleSlider extends Component {
       }
     };
 
+    const sliderComponent = (photo, altTag, description) => (
+      <div>
+        <img style={style.image} src={photo} alt={altTag} />
+        <p className="text-center">{description}</p>
+      </div>
+    );
+
     return (
       <Col>
         <Slider {...settings}>
-          <div>
-            <img style={style.image} src={ironmanPhoto} alt="ironman" />
-            <p>
-              I'm an ultra athlete, I've complete 7 marathons, won the Chi Town
-              Half Marathon and completed 2 Ironman competitions.
-            </p>
-          </div>
-          <div>
-            <img style={style.image} src={wrestlingClub} alt="wrestling club" />
-            <p>
-              In college I had the opportunity to be president and coach of the
-              University of Pittsburgh Wrestling Club. Here I had the chance to
-              lead a great team and wrestlers and win quite a few championships.
-            </p>
-          </div>
-          <div>
-            <img
-              style={style.image}
-              src={samaritansPurse}
-              alt="samaritans purse"
-            />
-            <p>
-              Over my trip across the country I signed up to become part of
-              Samaritan Purse's disaster relief team. We volunteered in Baton
-              Rouge to help the recent flood victims.
-            </p>
-          </div>
-          <div>
-            <img style={style.image} src={mountains} alt="mountains" />
-            <p>I love exploring the outdoors, climbing and mountaineering.</p>
-          </div>
-          <div>
-            <img
-              style={style.image}
-              src={highSchoolWrestling}
-              alt="high school wrestling"
-            />
-            <p>I was an All American high school wrestler</p>
-          </div>
+          {sliderComponent(
+            ironmanPhoto,
+            "ironman finish line",
+            "I'm an ultra athlete, I've complete 7 marathons, won the Chi Town Half Marathon and completed 2 Ironman competitions."
+          )}
+          {sliderComponent(
+            wrestlingClub,
+            "Wrestling Club",
+            "In college I had the opportunity to be president and coach of the University of Pittsburgh Wrestling Club. Here I had the chance to lead a great team of wrestlers and win a few championships."
+          )}
+          {sliderComponent(
+            samaritansPurse,
+            "Samaritans Purse volunteer group",
+            "Over my trip across the country I signed up to become part of Samaritan Purse's disaster relief team. We volunteered in Baton Rouge to help the recent flood victims."
+          )}
+          {sliderComponent(
+            mountains,
+            "Scott in the mountains",
+            "I love exploring the outdoors, climbing and mountaineering."
+          )}
+          {sliderComponent(
+            highSchoolWrestling,
+            "Medaling in the PA state wrestling competition",
+            "I was an All American high school wrestler."
+          )}
         </Slider>
       </Col>
     );
